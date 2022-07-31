ccc3b9a7719f38e8e81051f4e8c7ffe1ba4bfdce
MaherTheMaker <maher.serawan.1998@gmail.com>
2022-07-31 19:51:54 +0300

    MoneySafe V 0.3

--- spring-boot-jwt-25-7/src/main/java/com/Maker/model/ObjectAlreadyExist.java
+++ spring-boot-jwt-25-7/src/main/java/com/Maker/model/ObjectAlreadyExist.java
@@ -1,5 +1,9 @@
 package com.Maker.model;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class ObjectAlreadyExist extends RuntimeException {
 
         private String message;
